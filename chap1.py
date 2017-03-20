0996a3ed659059f32f408a3304139cbac47335fb
sgyy <wenyemin693@tom.com>
2017-03-20 15:23:43 +0800

    chap1_demo

---
+++ chap1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/en python
+# -*- coding: utf-8 -*-
+
+print("hello world")
+print("hello world2")
+print('hello world3')
+print('''hello world,
+      welcome to bluedon,
+      bluid you dream here''')
---
+++ chap1_project.py
@@ -0,0 +1,14 @@
+#!/usr/bin/en python
+# -*- coding: utf-8 -*-
+
+
+print('Hello world!')
+print('What is your name?')  # ask for their name
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?')  # ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+
