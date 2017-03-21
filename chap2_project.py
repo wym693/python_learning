4eaf0ed699b02863a5504c4197f88334ae4e5c5b
sgyy <wenyemin693@tom.com>
2017-03-21 09:16:18 +0800

    修复下雨实例2的bug

--- chap2_project.py
+++ chap2_project.py
@@ -1,31 +1,25 @@
-#!/usr/bin/en python
+﻿#!/usr/bin/en python
 # -*- coding: utf-8 -*-
-
-#使用if else elif 完成以下需求
-#是否下雨？是 --》有没有伞？--》没有--->等一会？有伞--走
-
 print('开始让计算机判断是否能走啦？')
 print("下雨么？只能回答yes/no")
 isRain=input()
 if isRain=='yes':
-	print("有伞么?只能回答yes/no")
-	hasUmbre=input()
-	if hasUmbre=='yes':
-		print("可以外出了！")
-	elif hasUmbre=="no":
-		print("只能等一会")
-		print("下雨么？只能回答yes/no")
-		isRain=input()
+    print("有伞么?只能回答yes/no")
+    hasUmbre=input()
+    if hasUmbre=='yes':
+        print("可以外出了！")
+    elif hasUmbre=="no":
+        print("只能等一会")
+        print("下雨么？只能回答yes/no")
+        isRain=input()
         while isRain=='yes':
-			print("只能等一会")
-			print("下雨么？只能回答yes/no")
-			isRain=input()
-			if isRain=='no':
-			break;
-		print("可以外出了！")
-elif isRain='no':
-	print("可以外出了！")
+            print("只能等一会")
+            print("下雨么？只能回答yes/no")
+            isRain=input()
+            if isRain=='no':
+                break
+            print("可以外出了！")
+elif isRain=='no':
+    print("可以外出了！")
 else:
     print("输入错误，程序退出")
-
-
